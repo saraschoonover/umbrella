98c8738a14044f56fe023f94d6be2c47f3a2cff9
Sara Schoonover <Saramschoon@gmail.com>
2023-08-03 14:43:14 +0000

    updated

--- weather.rb
+++ weather.rb
@@ -1,8 +1,6 @@
 require "http"
 require "json"
 
-GMAPS_KEY = "AIzaSyDKz4Y3bvrTsWpPRNn9ab55OkmcwZxLOHI"
-PIRATE_WEATHER_KEY = "3RrQrvLmiUayQ84JSxL8D2aXw99yRKlx1N4qFDUE"
 
 puts "Hello, where are you currently?"
 user_location = gets.chomp
